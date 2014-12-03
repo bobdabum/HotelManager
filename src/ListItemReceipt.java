d03b0f73bff44bb4b2180a4f59825522e59a53a3
Linye Ouyang <linye.ou@gmail.com>
2014-12-03 14:27:02 -0800

    minor change

--- src/ListItemReceipt.java
+++ src/ListItemReceipt.java
@@ -3,7 +3,6 @@ import java.text.SimpleDateFormat;
 
 import javax.swing.*;
 
-<<<<<<< HEAD
 @SuppressWarnings("serial")
 public class ListItemReceipt extends ListItem{
 		public ListItemReceipt(Reservation r,int position) {
@@ -16,25 +15,6 @@ public class ListItemReceipt extends ListItem{
 			add(new JLabel("Check-in: "+sf.format(r.getStart().getTime())));
 			add(new JLabel("Check-out: "+sf.format(r.getEnd().getTime())));
 			add(new JLabel("Cost: $"+r.getRoomCost().getCost()));
-=======
-
-public class ListItemReceipt implements ListItemType<Reservation> {
-	public ArrayList<JPanel> populateList(ArrayList<Reservation> myItems, ArrayList<Boolean> isSelected) {
-		ArrayList<JPanel> returnList = new ArrayList<JPanel>();
-		int total = 0;
-		SimpleDateFormat sf = new SimpleDateFormat("MM/DD/YYYY");
-		for(Reservation r: myItems){
-			total += r.getRoomCost().getCost();
-			JPanel newPanel = new JPanel();
-			newPanel.setLayout(new GridLayout(2,3));
-			newPanel.add(new JLabel("User ID: "+r.getUserID()));
-			newPanel.add(new JLabel("User Name: "+r.getUserName()));
-			newPanel.add(new JLabel("Room ID: "+r.getRoomID()));
-			newPanel.add(new JLabel("Check-in: "+sf.format(r.getStart().getTime())));
-			newPanel.add(new JLabel("Check-out: "+sf.format(r.getEnd().getTime())));
-			newPanel.add(new JLabel("Cost: $"+r.getRoomCost().getCost()));
-			returnList.add(newPanel);
->>>>>>> origin/Linye
 		}
 		
 	}
